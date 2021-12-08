e6e116884f44018123ff4f77890caf9959750a20
Kevin Böckler <kevinboeckler@gmail.com>
2021-12-08 07:45:33 +0100

    Day7 Part1

--- solution/day7.go
+++ solution/day7.go
@@ -1,6 +1,8 @@
 package solution
 
 import (
+	"math"
+	"sort"
 	"strconv"
 	"strings"
 )
@@ -15,11 +17,32 @@ func (d Day7) SolvePart1(input string) int {
 		number, _ := strconv.Atoi(numberAsString)
 		numbers = append(numbers, number)
 	}
-	
-	return 0
+	sort.Ints(numbers)
+	middleNumber := numbers[len(numbers)/2-1]
+	fuel := 0
+	for _, number := range numbers {
+		delta := int(math.Abs(float64(number - middleNumber)))
+		fuel = fuel + delta
+	}
+	return fuel
 }
 
 func (d Day7) SolvePart2(input string) int {
-	//TODO implement me
-	return 0
+	numbersAsString := strings.Split(input, ",")
+	numbers := make([]int, 0, len(numbersAsString))
+	for _, numberAsString := range numbersAsString {
+		number, _ := strconv.Atoi(numberAsString)
+		numbers = append(numbers, number)
+	}
+	sum := 0
+	for _, number := range numbers {
+		sum = sum + number
+	}
+	average := int(math.Round(float64(sum) / float64(len(numbers))))
+	fuel := 0
+	for _, number := range numbers {
+		delta := int(math.Abs(float64(number - average)))
+		fuel = fuel + delta
+	}
+	return fuel
 }
